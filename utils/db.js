99468da8d1ffe26e88599963297088f4aa780e25
Geepeey <harshmallins@gmail.com>
2023-09-06 23:35:17 +0000

    .

--- utils/db.js
+++ utils/db.js
@@ -1,70 +1,32 @@
-const { MongoClient } = require('mongodb');
-const mongo = require('mongodb');
-const { pwdHashed } = require('./utils');
+import { MongoClient } from 'mongodb';
 
 class DBClient {
-  constructor() {
-    const host = (process.env.DB_HOST) ? process.env.DB_HOST : 'localhost';
-    const port = (process.env.DB_PORT) ? process.env.DB_PORT : 27017;
-    this.database = (process.env.DB_DATABASE) ? process.env.DB_DATABASE : 'files_manager';
-    const dbUrl = `mongodb://${host}:${port}`;
-    this.connected = false;
-    this.client = new MongoClient(dbUrl, { useUnifiedTopology: true });
-    this.client.connect().then(() => {
-      this.connected = true;
-    }).catch((err) => console.log(err.message));
-  }
-
-  isAlive() {
-    return this.connected;
-  }
-
-  async nbUsers() {
-    await this.client.connect();
-    const users = await this.client.db(this.database).collection('users').countDocuments();
-    return users;
-  }
-
-  async nbFiles() {
-    await this.client.connect();
-    const users = await this.client.db(this.database).collection('files').countDocuments();
-    return users;
-  }
-
-  async createUser(email, password) {
-    const hashedPwd = pwdHashed(password);
-    await this.client.connect();
-    const user = await this.client.db(this.database).collection('users').insertOne({ email, password: hashedPwd });
-    return user;
-  }
-
-  async getUser(email) {
-    await this.client.connect();
-    const user = await this.client.db(this.database).collection('users').find({ email }).toArray();
-    if (!user.length) {
-      return null;
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}/${database}`;
+        this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        this.client.connect();
+        this.db = this.client.db(database);
     }
-    return user[0];
-  }
 
-  async getUserById(id) {
-    const _id = new mongo.ObjectID(id);
-    await this.client.connect();
-    const user = await this.client.db(this.database).collection('users').find({ _id }).toArray();
-    if (!user.length) {
-      return null;
+    isAlive() {
+        return this.client.isConnected();
     }
-    return user[0];
-  }
 
-  async userExist(email) {
-    const user = await this.getUser(email);
-    if (user) {
-      return true;
+    async nbUsers() {
+        const usersCollection = this.db.collection('users');
+        const userCount = await usersCollection.countDocuments();
+        return userCount;
     }
-    return false;
-  }
+
+    async nbFiles() {
+        const filesCollection = this.db.collection('files');
+        const fileCount = await filesCollection.countDocuments();
+        return fileCount;
+    }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
